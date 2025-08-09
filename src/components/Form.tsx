bf023763ee3f290c68d89bdd69ce4336ed09cdcc
cesar91bo <cesar91bo@gmail.com>
2025-08-09 14:31:45 -0300

    agregué una librería de generador único y lo usé en el componente

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,17 +2,22 @@ import { categories } from "../data/categories";
 import { useState, type ChangeEvent, type FormEvent, useReducer, type Dispatch } from "react";
 import type { Activity } from "../types";
 import type { ActivityAction } from "../reducers/activityReducer";
+import { v4 as uuidv4 } from 'uuid'
 
 type FormProps = {
   dispatch: Dispatch<ActivityAction>
 }
+
+const initialState : Activity = {
+    id: uuidv4(),
+    category: 1,
+    name: '',
+    calories: 0
+}
+
 export default function Form({ dispatch }: FormProps) {
 
-    const [activity, setActivity] = useState<Activity>({
-        category: 1,
-        name: '',
-        calories: 0
-    })
+    const [activity, setActivity] = useState<Activity>(initialState)
 
     const handleChange = (e:  ChangeEvent<HTMLSelectElement | HTMLInputElement>)=> {
         const isNumberField = ['category', 'calories'].includes(e.target.id);
@@ -31,6 +36,11 @@ export default function Form({ dispatch }: FormProps) {
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         dispatch({type:'save-activity', payload:{newActivity: activity}})
+
+        setActivity({
+            ...initialState,
+            id: uuidv4()
+        })
     }
 
   return (
