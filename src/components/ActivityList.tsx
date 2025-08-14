86c3273999cfa7403cbfb5b7788eff37d8108e2d
cesar91bo <cesar91bo@gmail.com>
2025-08-14 10:03:15 -0300

    Creación del botón eliminar y su lógica

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,7 +1,7 @@
 import type { Activity } from "../types"
 import { useMemo, type Dispatch } from "react"
 import { categories } from "../data/categories"
-import {PencilSquareIcon} from '@heroicons/react/24/outline'
+import {PencilSquareIcon, TrashIcon} from '@heroicons/react/24/outline'
 import type { ActivityAction } from "../reducers/activityReducer"
 
 type ActivityListProps = {
@@ -15,11 +15,15 @@ export default function ActivityList({ activities, dispatch }: ActivityListProps
         (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : '')
         ,[activities])
 
+        const isEmptyActivity = useMemo(() => activities.length === 0, [activities])
+
   return (
     <>
         <h2 className="text-4xl font-bold text-slate-600 text-center">Comida y Actividades</h2>
 
-        {activities.map(activity => (
+        { isEmptyActivity?
+            <p className="text-center my-5">No hay actividades registradas</p> :
+            activities.map(activity => (
             <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
                 <div className="space-y-2 relative">
                     <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity
@@ -39,10 +43,15 @@ export default function ActivityList({ activities, dispatch }: ActivityListProps
                             className="h-8 w-8 text-gray-800"
                         />
                     </button>
+                    <button
+                        onClick={() => dispatch({ type: 'delete-activity', payload: { id: activity.id } })}
+                    >
+                        <TrashIcon className="h-8 w-8 text-red-500" />
+                    </button>
                 </div>
             </div>
         ))}
     </>
 
-  )
+  ) 
 }
--- src/reducers/activityReducer.ts
+++ src/reducers/activityReducer.ts
@@ -2,7 +2,8 @@ import type { Activity } from "../types"
 
 export type ActivityAction = 
 {type: 'save-activity', payload:{newActivity: Activity}} |
-{type: 'set-activeId', payload:{id: Activity['id']}}
+{type: 'set-activeId', payload:{id: Activity['id']}} |
+{type: 'delete-activity', payload:{id: Activity['id']}}
 
 export type ActivityState = {
     activities: Activity[],
@@ -42,6 +43,14 @@ export const activityReducer = (
     return {
         ...state,
         activeId: action.payload.id
+    }
+ }
+
+ if(action.type === 'delete-activity'){
+    return {
+        ...state,
+        activities: state.activities.filter(activity => activity.id !== action.payload.id),
+        activeId: ''
     }
  }
 
