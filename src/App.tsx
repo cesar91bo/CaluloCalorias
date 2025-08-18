3a4e840dedadc561878c872a168ffdd69bc28e32
cesar91bo <cesar91bo@gmail.com>
2025-08-18 20:49:20 -0300

    Componente para mostrar el total de calorías

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Form from "./components/Form"
 import { useReducer, useEffect, useMemo } from "react"
 import { activityReducer, initialState } from "./reducers/activityReducer"
 import ActivityList from "./components/ActivityList"
+import CalorieTracker from "./components/CalorieTracker"
 
 
 export default function App() {
@@ -37,6 +38,14 @@ export default function App() {
         <Form
           dispatch={dispatch}
           state={state}
+        />
+      </div>
+    </section>
+
+    <section className="bg-gray-800 py-10">
+      <div className="max-w-4xl mx-auto">
+        <CalorieTracker
+          activities={state.activities}
         />
       </div>
     </section>
---
+++ src/components/CalorieTracker.tsx
@@ -0,0 +1,10 @@
+import type { Activity } from "../types";
+
+type CaloriesTrackerProps ={
+  activities: Activity[]
+}
+export default function CalorieTracker({ activities }: CaloriesTrackerProps) {
+  return (
+    <div>CaloriesTracker</div>
+  )
+}
